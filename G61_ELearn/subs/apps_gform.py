f97c84e602f6ff71c5472f39e62f458488f73120
Goncalo-b <up202506624@up.pt>
2026-06-02 14:53:06 +0100

    primeira submissão | segunda parte

---
+++ G61_ELearn/subs/apps_gform.py
@@ -0,0 +1,92 @@
+from flask import render_template, request, session
+from classes.platform_class import Platform
+from classes.certificate import Certificate
+from classes.course import Course
+from classes.person import Person
+from classes.transaction import Trans
+
+prev_option = ""
+
+def apps_gform(cname=''):
+    global prev_option
+    ulogin = session.get("user")
+    if ulogin is None:
+        return render_template("index.html", ulogin=None)
+
+    cl      = eval(cname)
+    butshow = "enabled"
+    butedit = "disabled"
+    option  = request.args.get("option", "")
+    msg     = ""
+
+    if prev_option == 'insert' and option == 'save':
+        try:
+            strobj = request.form.get(cl.att[0], '0')
+            for i in range(1, len(cl.att)):
+                strobj += ";" + request.form.get(cl.att[i], '')
+            obj = cl.from_string(strobj)
+            cl.insert(getattr(obj, cl.att[0]))
+            cl.last()
+            msg = "ok:Registo inserido com sucesso."
+        except Exception as e:
+            msg = f"err:Erro ao inserir: {e}"
+
+    elif prev_option == 'edit' and option == 'save':
+        try:
+            obj = cl.current()
+            for i in range(1, len(cl.att)):
+                setattr(obj, cl.att[i], request.form.get(cl.att[i], ''))
+            cl.update(getattr(obj, cl.att[0]))
+            msg = "ok:Registo atualizado com sucesso."
+        except Exception as e:
+            msg = f"err:Erro ao atualizar: {e}"
+
+    else:
+        if option == "edit":
+            butshow, butedit = "disabled", "enabled"
+        elif option == "delete":
+            try:
+                obj = cl.current()
+                cl.remove(obj.id)
+                if not cl.previous():
+                    cl.first()
+                msg = "ok:Registo eliminado."
+            except Exception as e:
+                msg = f"err:Erro ao eliminar: {e}"
+        elif option == "insert":
+            butshow, butedit = "disabled", "enabled"
+        elif option == "goto":
+            try:
+                sid = int(request.args.get("search_id", 0))
+                if sid in cl.obj:
+                    cl.current(sid)
+                else:
+                    msg = "err:ID não encontrado."
+            except:
+                msg = "err:ID inválido."
+        elif option in ("first","previous","next","last","cancel"):
+            if option == "first":    cl.first()
+            elif option == "previous": cl.previous()
+            elif option == "next":   cl.nextrec()
+            elif option == "last":   cl.last()
+        elif option == 'exit':
+            return render_template("index.html", ulogin=ulogin)
+
+    prev_option = option
+    obj = cl.current()
+
+    if option == 'insert' or len(cl.lst) == 0:
+        obj = {cl.att[0]: cl.get_id(0)}
+        for i in range(1, len(cl.att)):
+            obj[cl.att[i]] = ""
+
+    recnum = (cl.pos + 1) if len(cl.lst) > 0 else 0
+    total  = len(cl.lst)
+
+    return render_template("gform.html",
+                           butshow=butshow, butedit=butedit,
+                           cname=cname, obj=obj,
+                           att=cl.att, des=cl.des,
+                           header=cl.header,
+                           recnum=recnum, total=total,
+                           msg=msg, ulogin=ulogin)
