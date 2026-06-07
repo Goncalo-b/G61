9a698ecf0e6ee93aeb98ec9c85f9039f915ed1e6
pedrolima <up202506953@up.pt>
2026-06-07 17:37:52 +0100

    Alteração final classes Course e Certificate

--- G61_ELearn/classes/certificate.py
+++ G61_ELearn/classes/certificate.py
@@ -1,31 +1,52 @@
-"""
-@author: Grupo 61 - FEUP PC II 2025/2026
-objective: class Certificate
-"""
-from collections import defaultdict, Counter
 from classes.gclass import Gclass
 
 class Certificate(Gclass):
     obj     = dict()
+    _by_id  = dict()
     lst     = list()
     pos     = 0
     sortkey = ''
-    att     = ['_id', '_certificate_name', '_certificate_type', '_certificate_language']
+    att     = ['_id', '_certificate_name', '_certificate_type', '_certificate_language', '_issue_date']
     header  = 'Certificate'
-    des     = ['Id', 'Name', 'Type', 'Language']
+    des     = ['Id', 'Name', 'Type', 'Language', 'Issue Date']
+    _seq    = 0
 
-    def __init__(self, id, certificate_name, certificate_type, certificate_language=''):
+    def __init__(self, id, certificate_name, certificate_type,
+                 certificate_language='', issue_date=''):
         super().__init__()
-        id = Certificate.get_id(int(float(id)))
-        self._id                   = id
+        Certificate._seq += 1
+        self._rowkey               = Certificate._seq
+        self._id                   = Certificate.get_id(int(float(id)))
         self._certificate_name     = str(certificate_name)
         self._certificate_type     = str(certificate_type)
         self._certificate_language = str(certificate_language)
-        Certificate.obj[id] = self
-        Certificate.lst.append(id)
+        if str(issue_date) not in ('', 'None'):
+            self._issue_date = str(issue_date)[:10]
+        else:
+            self._issue_date = ''
+        Certificate.obj[self._rowkey] = self
+        Certificate.lst.append(self._rowkey)
+        if self._id not in Certificate._by_id:
+            Certificate._by_id[self._id] = self
+
+    @classmethod
+    def get_by_id(cls, db_id):
+        return cls._by_id.get(int(db_id))
 
     @property
     def id(self): return self._id
+
+    def get_key(self): return self._rowkey
+
+    @classmethod
+    def get_id(cls, id):
+        id = int(id)
+        if id == 0:
+            existing = list(cls._by_id.keys())
+            if existing:
+                return max(existing) + 1
+            return 1
+        return id
 
     @property
     def certificate_name(self): return self._certificate_name
@@ -42,44 +63,159 @@ class Certificate(Gclass):
     @certificate_language.setter
     def certificate_language(self, v): self._certificate_language = str(v)
 
+    @property
+    def issue_date(self): return self._issue_date
+    @issue_date.setter
+    def issue_date(self, v):
+        if str(v) not in ('', 'None'):
+            self._issue_date = str(v)[:10]
+        else:
+            self._issue_date = ''
+
+    @classmethod
+    def read(cls, path=''):
+        cls.obj    = dict()
+        cls._by_id = dict()
+        cls.lst    = list()
+        cls._seq   = 0
+        cls.pos    = 0
+        cls.path   = path
+        try:
+            rows = cls.sqlexe("SELECT * FROM Certificate ORDER BY id")
+            if rows:
+                for r in rows:
+                    cls(*r)
+        except Exception as err:
+            print('Certificate.read error: ' + str(err))
+
+    @classmethod
+    def remove(cls, p):
+        obj = cls.obj[p]
+        cls.sqlexe(
+            "DELETE FROM Certificate WHERE rowid = "
+            "(SELECT rowid FROM Certificate WHERE id=" + str(obj._id) + " LIMIT 1)"
+        )
+        cls.lst.remove(p)
+        del cls.obj[p]
+        cls._by_id.pop(obj._id, None)
+        for o in cls.obj.values():
+            if o._id == obj._id:
+                cls._by_id[obj._id] = o
+                break
+
+    @classmethod
+    def insert(cls, p):
+        obj = cls.obj[p]
+        cls.sqlexe(
+            'INSERT INTO Certificate VALUES(' +
+            str(obj._id) + ',"' + obj._certificate_name + '","' +
+            obj._certificate_type + '","' + obj._certificate_language +
+            '","' + obj._issue_date + '")'
+        )
+
+    @classmethod
+    def update(cls, p):
+        obj = cls.obj[p]
+        cls.sqlexe(
+            'UPDATE Certificate SET '
+            'certificate_name="' + obj._certificate_name + '", '
+            'certificate_type="' + obj._certificate_type + '", '
+            'certificate_language="' + obj._certificate_language + '", '
+            'issue_date="' + obj._issue_date + '" '
+            'WHERE rowid=(SELECT rowid FROM Certificate WHERE id=' + str(obj._id) + ' LIMIT 1)'
+        )
+
+    @classmethod
+    def issued_by_year(cls):
+        counts = {}
+        for c in cls.obj.values():
+            if c._issue_date:
+                year = c._issue_date[:4]
+                if year not in counts:
+                    counts[year] = 0
+                counts[year] += 1
+
+        result = []
+        for year in sorted(counts.keys()):
+            result.append((year, counts[year]))
+        return result
 
     @classmethod
     def most_issued(cls, trans_obj, top_n=10):
-        counts = Counter(t._certificate_id for t in trans_obj.values())
+        counts = {}
+        for t in trans_obj.values():
+            cid = t._certificate_id
+            if cid not in counts:
+                counts[cid] = 0
+            counts[cid] += 1
+
+        pairs = []
+        for cid, count in counts.items():
+            pairs.append((cid, count))
+        pairs.sort(key=lambda x: x[1], reverse=True)
+
         result = []
-        for cid, count in counts.most_common(top_n):
-            name = cls.obj[cid].certificate_name if cid in cls.obj else f'Cert {cid}'
-            ctype = cls.obj[cid].certificate_type if cid in cls.obj else ''
+        for cid, count in pairs[:top_n]:
+            obj = cls.get_by_id(cid)
+            if obj:
+                name  = obj._certificate_name
+                ctype = obj._certificate_type
+            else:
+                name  = 'Cert ' + str(cid)
+                ctype = ''
             result.append({'id': cid, 'name': name, 'type': ctype, 'count': count})
         return result
 
     @classmethod
     def distribution_by_type(cls):
-        counts = Counter(c.certificate_type for c in cls.obj.values())
-        return sorted(counts.items(), key=lambda x: x[1], reverse=True)
+        counts = {}
+        for c in cls.obj.values():
+            ctype = c._certificate_type
+            if ctype not in counts:
+                counts[ctype] = 0
+            counts[ctype] += 1
+
+        pairs = []
+        for ctype, count in counts.items():
+            pairs.append((ctype, count))
+        pairs.sort(key=lambda x: x[1], reverse=True)
+        return pairs
 
     @classmethod
     def distribution_by_language(cls):
-        counts = Counter(c.certificate_language for c in cls.obj.values())
-        return sorted(counts.items(), key=lambda x: x[1], reverse=True)
+        counts = {}
+        for c in cls.obj.values():
+            lang = c._certificate_language
+            if lang not in counts:
+                counts[lang] = 0
+            counts[lang] += 1
+
+        pairs = []
+        for lang, count in counts.items():
+            pairs.append((lang, count))
+        pairs.sort(key=lambda x: x[1], reverse=True)
+        return pairs
 
     @classmethod
     def avg_fee_by_type(cls, trans_obj):
-        totals = defaultdict(float)
-        counts = defaultdict(int)
+        totals = {}
+        counts = {}
         for t in trans_obj.values():
-            if t._certificate_id in cls.obj:
-                ctype = cls.obj[t._certificate_id].certificate_type
+            obj = cls.get_by_id(t._certificate_id)
+            if obj:
+                ctype = obj._certificate_type
+                if ctype not in totals:
+                    totals[ctype] = 0.0
+                    counts[ctype] = 0
                 totals[ctype] += t._certificate_fee
                 counts[ctype] += 1
+
         result = []
         for ctype in totals:
-            result.append({
-                'type': ctype,
-                'avg_fee': round(totals[ctype] / counts[ctype], 2),
-                'count': counts[ctype]
-            })
-        return sorted(result, key=lambda x: x['avg_fee'], reverse=True)
+            avg = round(totals[ctype] / counts[ctype], 2)
+            result.append({'type': ctype, 'avg_fee': avg, 'count': counts[ctype]})
+        result.sort(key=lambda x: x['avg_fee'], reverse=True)
+        return result
 
     def __str__(self):
-        return f'Id:{self._id}, Name:{self._certificate_name}, Type:{self._certificate_type}'
+        return 'Id:' + str(self._id) + ', Name:' + self._certificate_name + ', Type:' + self._certificate_type
--- G61_ELearn/classes/course.py
+++ G61_ELearn/classes/course.py
@@ -1,8 +1,3 @@
-"""
-@author: Grupo 61 - FEUP PC II 2025/2026
-objective: class Course
-"""
-from collections import Counter
 from classes.gclass import Gclass
 
 class Course(Gclass):
@@ -13,21 +8,40 @@ class Course(Gclass):
     att     = ['_id', '_course_name', '_course_category', '_course_info', '_online_date', '_platform_id']
     header  = 'Course'
     des     = ['Id', 'Name', 'Category', 'Info', 'Online Date', 'Platform Id']
+    _seq    = 0
 
     def __init__(self, id, course_name, course_category, course_info, online_date, platform_id):
         super().__init__()
-        id = Course.get_id(int(float(id)))
-        self._id              = id
+        Course._seq += 1
+        self._rowkey          = Course._seq
+        self._id              = Course.get_id(int(float(id)))
         self._course_name     = str(course_name)
         self._course_category = str(course_category)
         self._course_info     = str(course_info)
         self._online_date     = str(online_date)
-        self._platform_id     = int(float(platform_id))
-        Course.obj[id] = self
-        Course.lst.append(id)
+        if str(platform_id).strip() not in ('', 'None'):
+            self._platform_id = int(float(platform_id))
+        else:
+            self._platform_id = 0
+        Course.obj[self._rowkey] = self
+        Course.lst.append(self._rowkey)
 
     @property
     def id(self): return self._id
+
+    def get_key(self): return self._rowkey
+
+    @classmethod
+    def get_id(cls, id):
+        id = int(id)
+        if id == 0:
+            existing = []
+            for o in cls.obj.values():
+                existing.append(o._id)
+            if existing:
+                return max(existing) + 1
+            return 1
+        return id
 
     @property
     def course_name(self): return self._course_name
@@ -54,26 +68,99 @@ class Course(Gclass):
     @platform_id.setter
     def platform_id(self, v): self._platform_id = int(float(v))
 
+    @classmethod
+    def read(cls, path=''):
+        cls.obj  = dict()
+        cls.lst  = list()
+        cls._seq = 0
+        cls.pos  = 0
+        cls.path = path
+        try:
+            rows = cls.sqlexe("SELECT * FROM Course ORDER BY id")
+            if rows:
+                for r in rows:
+                    cls(*r)
+        except Exception as err:
+            print('Course.read error: ' + str(err))
+
+    @classmethod
+    def remove(cls, p):
+        obj = cls.obj[p]
+        cls.sqlexe(
+            "DELETE FROM Course WHERE rowid = "
+            "(SELECT rowid FROM Course WHERE id=" + str(obj._id) + " LIMIT 1)"
+        )
+        cls.lst.remove(p)
+        del cls.obj[p]
+
+    @classmethod
+    def insert(cls, p):
+        obj = cls.obj[p]
+        cls.sqlexe(
+            'INSERT INTO Course VALUES(' +
+            str(obj._id) + ',"' + obj._course_name + '","' +
+            obj._course_category + '","' + obj._course_info +
+            '","' + obj._online_date + '",' + str(obj._platform_id) + ')'
+        )
+
+    @classmethod
+    def update(cls, p):
+        obj = cls.obj[p]
+        cls.sqlexe(
+            'UPDATE Course SET '
+            'course_name="' + obj._course_name + '", '
+            'course_category="' + obj._course_category + '", '
+            'course_info="' + obj._course_info + '", '
+            'online_date="' + obj._online_date + '", '
+            'platform_id=' + str(obj._platform_id) + ' '
+            'WHERE rowid=(SELECT rowid FROM Course WHERE id=' + str(obj._id) + ' LIMIT 1)'
+        )
 
     @classmethod
     def distribution_by_category(cls):
-        counts = Counter(c.course_category for c in cls.obj.values())
-        return sorted(counts.items(), key=lambda x: x[1], reverse=True)
+        counts = {}
+        for c in cls.obj.values():
+            cat = c._course_category
+            if cat not in counts:
+                counts[cat] = 0
+            counts[cat] += 1
+
+        pairs = []
+        for cat, count in counts.items():
+            pairs.append((cat, count))
+        pairs.sort(key=lambda x: x[1], reverse=True)
+        return pairs
 
     @classmethod
     def courses_per_platform(cls, top_n=10):
-        counts = Counter(c.platform_id for c in cls.obj.values())
-        return counts.most_common(top_n)
+        counts = {}
+        for c in cls.obj.values():
+            pid = c._platform_id
+            if pid not in counts:
+                counts[pid] = 0
+            counts[pid] += 1
+
+        pairs = []
+        for pid, count in counts.items():
+            pairs.append((pid, count))
+        pairs.sort(key=lambda x: x[1], reverse=True)
+        return pairs[:top_n]
 
     @classmethod
     def courses_added_by_year(cls):
-        counts = Counter()
+        counts = {}
         for c in cls.obj.values():
-            if c.online_date and c.online_date != 'None':
-                year = str(c.online_date)[:4]
+            if c._online_date and c._online_date != 'None':
+                year = str(c._online_date)[:4]
                 if year.isdigit():
+                    if year not in counts:
+                        counts[year] = 0
                     counts[year] += 1
-        return sorted(counts.items())
+
+        result = []
+        for year in sorted(counts.keys()):
+            result.append((year, counts[year]))
+        return result
 
     def __str__(self):
-        return f'Id:{self._id}, Name:{self._course_name}, Category:{self._course_category}'
+        return 'Id:' + str(self._id) + ', Name:' + self._course_name + ', Category:' + self._course_category
